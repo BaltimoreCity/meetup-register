0259920446b1db7348a465d842237db49121a1bb
Tony Ling <no1_3000@hotmail.com>
2019-01-28 14:25:22 +0800

    update

--- index.js
+++ index.js
@@ -1,58 +1,91 @@
-const https = require("https");
-const mkdirp = require("mkdirp");
+const request = require("request-promise");
 const moment = require("moment");
-const yaml = require("write-yaml");
-const { strip_html_tags } = require("./utils");
-const meetups = require("./links.json");
+const YAML = require("yamljs");
+const { stripHtmlTags, getPath } = require("./utils");
+const eventLinks = require("./links.json");
 
-const API_ROOT = "https://api.meetup.com/";
-const EVENTS_DATA_DIR = "_data/events";
-const regex = /(?<=meetup\.com\/)(.*)/g;
+const MEETUP_API_ROOT = "https://api.meetup.com";
+const GITHUB_API_ROOT = "https://api.github.com";
+const GITHUB_REPO = "no13000/testwrite2github";
+const GITHUB_TOKEN = "50769df28cdd83a7a062ae80d0e74b4603e97afb";
 
-function callApi(url) {
-  https.get(url, res => {
-    let data = "";
-    res.on("data", chunk => (data += chunk));
+// create/update data file in github repo
+async function createEventFileInRepo(path, data) {
+  const url = `${GITHUB_API_ROOT}/repos/${GITHUB_REPO}/contents/${path}`;
 
-    res.on("end", () => {
-      const event = JSON.parse(data);
-      // write data to event data file
+  const sha = await getFileSha(path);
+
+  return request(url, {
+    headers: {
+      "User-Agent": "meetup-register-put",
+      Accept: "application/vnd.github.v3+json",
+      Authorization: `token ${GITHUB_TOKEN}`
+    },
+    method: "PUT",
+    json: true,
+    body: {
+      message: "[Lambda] create event file",
+      content: Buffer.from(YAML.stringify(data)).toString("base64"),
+      sha
+    }
+  })
+    .then(data => console.log(`update ${data.content.name} success`))
+    .catch(err => err.message);
+}
+
+// get file sha through github api [GET]
+function getFileSha(path) {
+  const url = `${GITHUB_API_ROOT}/repos/${GITHUB_REPO}/contents/${path}`;
+
+  return request(url, {
+    headers: {
+      "User-Agent": "meetup-register-get",
+      Accept: "application/vnd.github.v3+json",
+      Authorization: `token ${GITHUB_TOKEN}`
+    },
+    json: true
+  })
+    .then(data => data.sha)
+    .catch(err => null);
+}
+
+// get event info through meetup api
+async function getEvent(link) {
+  const path = getPath(link);
+  if (path) {
+    const success = async body => {
+      const event = JSON.parse(body);
       if (event) {
         const eventStart = moment.utc(event.time);
         const eventEnd = moment.utc(event.time).add(event.duration);
-        const output_dir = `${EVENTS_DATA_DIR}/${eventStart.year()}/${eventStart.month() +
-          1}`;
+        // outputPath format `_data/events/{year}/{month}/{id}.yml`
+        const outputPath = `_data/events/${eventStart.year()}/${eventStart.month() +
+          1}/${event.id}.yml`;
 
-        mkdirp(output_dir, function(err) {
-          if (err) console.error(err);
-          else {
-            const output = {
-              start: eventStart.format(),
-              end: eventEnd.format(),
-              location: event.venue.name || "",
-              title: event.name,
-              type: "meetup",
-              summary: strip_html_tags(event.description),
-              link: event.link
-            };
+        const data = {
+          start: eventStart.format(),
+          end: eventEnd.format(),
+          location: event.venue.name || "",
+          title: event.name,
+          type: "meetup",
+          summary: stripHtmlTags(event.description),
+          link: event.link
+        };
 
-            // generate data file as yml
-            yaml(`${output_dir}/${event.id}.yml`, output, function(err) {
-              if (err) {
-                console.error(err);
-              }
-            });
-          }
-        });
+        await createEventFileInRepo(outputPath, data);
       }
-    });
-  });
+    };
+
+    await request(MEETUP_API_ROOT + "/" + path)
+      .then(success)
+      .catch(err => console.log(`meetup api error`));
+  }
 }
 
-meetups.forEach(e => {
-  const match = e.match(regex) || [];
-
-  if (match[0]) {
-    callApi(API_ROOT + match[0]);
+async function run() {
+  for (const link of eventLinks) {
+    await getEvent(link);
   }
-});
+}
+
+run();
