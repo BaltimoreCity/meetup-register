b2839fc96d5393d0e44fb3e214861bd1074c1b7a
Tony Ling <no1_3000@hotmail.com>
2019-01-29 12:33:40 +0800

    cleanup codes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const request = require("request-promise");
 const moment = require("moment");
 const YAML = require("yamljs");
 const { stripHtmlTags, getPath } = require("./utils");
-const eventLinks = require("./links.json");
+const dataUrls = require("./data/index.json");
 
 const MEETUP_API_ROOT = "https://api.meetup.com";
 const GITHUB_API_ROOT = "https://api.github.com";
@@ -10,82 +10,104 @@ const GITHUB_REPO = "";
 const GITHUB_TOKEN = "";
 
 // create/update data file in github repo
-async function createEventFileInRepo(path, data) {
+function createEventFileInRepo(path, data, sha = null) {
   const url = `${GITHUB_API_ROOT}/repos/${GITHUB_REPO}/contents/${path}`;
-
-  const sha = await getFileSha(path);
-
+  const content = Buffer.from(YAML.stringify(data)).toString("base64");
   return request(url, {
     headers: {
-      "User-Agent": "meetup-register-put",
+      "User-Agent": "meetup-register-update",
       Accept: "application/vnd.github.v3+json",
       Authorization: `token ${GITHUB_TOKEN}`
     },
     method: "PUT",
     json: true,
     body: {
-      message: "[Lambda] create event file",
-      content: Buffer.from(YAML.stringify(data)).toString("base64"),
+      message: "Update event data file",
+      content,
       sha
     }
   })
-    .then(data => console.log(`update ${data.content.name} success`))
-    .catch(err => console.error(err.message));
+    .then(data => [null, data])
+    .catch(error => [error, null]);
 }
 
-// get file sha through github api [GET]
+// get file sha through github api [GET], required in order to update existing data files
 function getFileSha(path) {
   const url = `${GITHUB_API_ROOT}/repos/${GITHUB_REPO}/contents/${path}`;
 
   return request(url, {
     headers: {
-      "User-Agent": "meetup-register-get",
+      "User-Agent": "meetup-register-sha",
       Accept: "application/vnd.github.v3+json",
       Authorization: `token ${GITHUB_TOKEN}`
     },
     json: true
   })
-    .then(data => data.sha)
-    .catch(err => null);
+    .then(data => [null, data.sha])
+    .catch(error => [error, null]);
 }
 
 // get event info through meetup api
-async function getEvent(link) {
-  const path = getPath(link);
-  if (path) {
-    const success = async body => {
-      const event = JSON.parse(body);
-      if (event) {
-        const eventStart = moment.utc(event.time);
-        const eventEnd = moment.utc(event.time).add(event.duration);
-        // outputPath format `_data/events/{year}/{month}/{id}.yml`
-        const outputPath = `_data/events/${eventStart.year()}/${eventStart.month() +
-          1}/${event.id}.yml`;
+function getEvent(url) {
+  const path = getPath(url);
+  return request(MEETUP_API_ROOT + "/" + path)
+    .then(body => [null, JSON.parse(body)])
+    .catch(error => [error, null]);
+}
 
-        const data = {
-          start: eventStart.format(),
-          end: eventEnd.format(),
-          location: event.venue.name || "",
-          title: event.name,
-          type: "meetup",
-          summary: stripHtmlTags(event.description),
-          link: event.link
-        };
+async function writeEventsFromUrls(urls) {
+  for (const url of urls) {
+    const [getEventError, data] = await getEvent(url);
+    if (getEventError) {
+      // write error to data/failure folder
+      throw new Error(`Error in get event: ${getEventError}`);
+    }
 
-        await createEventFileInRepo(outputPath, data);
+    if (data) {
+      const eventStart = moment.utc(data.time),
+        eventEnd = moment.utc(data.time).add(data.duration);
+      const event = {
+        start: eventStart.format(),
+        end: eventEnd.format(),
+        location: data.venue.name || "",
+        title: data.name,
+        type: "meetup",
+        summary: stripHtmlTags(data.description),
+        link: data.link
+      };
+      const path = `_data/events/${eventStart.year()}/${eventEnd.month() + 1}/${
+        data.id
+      }.yml`;
+
+      // get sha
+      const [shaRequestError, sha] = await getFileSha(path);
+
+      if (shaRequestError) {
+        reportError(shaRequestError);
       }
-    };
 
-    await request(MEETUP_API_ROOT + "/" + path)
-      .then(success)
-      .catch(err => console.log(err));
+      // create/update file
+      const [updateFileRequestError, response] = await createEventFileInRepo(
+        path,
+        event,
+        sha
+      );
+
+      if (updateFileRequestError) {
+        reportError(updateFileRequestError);
+      }
+
+      if (response) {
+        console.log(
+          `Commit: ${response.commit.message}, sha: ${response.commit.sha}`
+        );
+      }
+    }
   }
 }
 
-async function run() {
-  for (const link of eventLinks) {
-    await getEvent(link);
-  }
+function start() {
+  writeEventsFromUrls(dataUrls);
 }
 
-run();
+start();
